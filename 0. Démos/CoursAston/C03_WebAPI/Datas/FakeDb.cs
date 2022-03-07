e65c9c98e8b6b24dc3ebcabe6d085e98928d8f8c
GharrowBM <gharrow.games@gmail.com>
2022-03-07 14:37:45 +0100

    Notions pour Exo 04

---
+++ 0. Démos/CoursAston/C03_WebAPI/Datas/FakeDb.cs
@@ -0,0 +1,52 @@
+﻿using C03_WebAPI.Models;
+
+namespace C03_WebAPI.Datas
+{
+    public class FakeDb
+    {
+        private List<Dog> _dogs;
+
+        public FakeDb()
+        {
+            _dogs = new();
+        }
+
+        public List<Dog> GetAll()
+        {
+            return _dogs;
+        }
+
+        public Dog GetById(int id)
+        {
+            return _dogs.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Dog AddDog(Dog dog)
+        {
+            _dogs.Add(dog);
+            return dog;
+        }
+
+        public bool DeleteDog(int id)
+        {
+            Dog toDelete = GetById(id);
+
+            if (toDelete == null) return false;
+
+            return _dogs.Remove(toDelete);
+        }
+
+        public Dog EditDog(int id, Dog newDog)
+        {
+            var toEdit = GetById(id);
+
+            if (toEdit == null) return null;
+
+            toEdit.Name = newDog.Name;
+            toEdit.NbOfLegs = newDog.NbOfLegs;
+            toEdit.CollarColor = newDog.CollarColor;
+
+            return toEdit;
+        }
+    }
+}
---
+++ 0. Démos/CoursAston/C03_WebAPI/Models/Dog.cs
@@ -0,0 +1,16 @@
+﻿namespace C03_WebAPI.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NbOfLegs { get; set; }
+        public string CollarColor { get; set; }
+
+        public static int Count;
+        public Dog()
+        {
+            Id = ++Count;
+        }
+    }
+}
