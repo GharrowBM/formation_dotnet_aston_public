24aba398f042b600a26c03999dc10160fe2b4b71
GharrowBM <gharrow.games@gmail.com>
2022-03-08 13:25:47 +0100

    Ajout correction Exo 04 et sujet Exo 05

---
+++ 3. ASP.NET et Applications Webs/2. Exercices/SolutionExercices/TP04/Datas/ContactsRepository.cs
@@ -0,0 +1,58 @@
+﻿using TP04.Models;
+
+namespace TP04.Datas
+{
+    public class ContactsRepository : BaseRepository, IRepository<Contact>
+    {
+        public ContactsRepository(AppplicationDbContext context) : base(context)
+        {
+        }
+
+        public Contact Add(Contact entity)
+        {
+            _context.Contacts.Add(entity);
+            if (_context.SaveChanges() > 0)
+            {
+                return entity;
+            }
+
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            _context.Contacts.Remove(GetById(id));
+
+            return _context.SaveChanges() > 0;
+        }
+
+        public List<Contact> GetAll()
+        {
+            return _context.Contacts.ToList();
+        }
+
+        public Contact GetById(int id)
+        {
+            return _context.Contacts.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Contact Update(int id, Contact entity)
+        {
+            Contact contactToEdit = GetById(id);
+
+            if (contactToEdit != null)
+            {
+                contactToEdit.Firstname = entity.Firstname;
+                contactToEdit.Lastname = entity.Lastname;
+                contactToEdit.Email = entity.Email;
+                contactToEdit.Phone = entity.Phone;
+                contactToEdit.AvatarURL = entity.AvatarURL;
+                contactToEdit.Password = entity.Password;
+
+                _context.SaveChanges();
+            }
+
+            return contactToEdit;
+        }
+    }
+}
