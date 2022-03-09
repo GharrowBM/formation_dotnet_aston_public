b8f0ebd7e91f93d69c0eb6a777894c471f09f00d
GharrowBM <gharrow.games@gmail.com>
2022-03-09 16:53:43 +0100

    push du soir TP 05

---
+++ 3. ASP.NET et Applications Webs/2. Exercices/SolutionExercices/TP05/Services/PasswordService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace TP05.Services
+{
+    public class PasswordService
+    {
+        private const string SecurityKey = "Clé de cryptage des mots de passe = salt";
+        public string EncryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return "";
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(password + SecurityKey));
+        }
+
+        public string DecryptPassword(string cryptedString)
+        {
+            if (string.IsNullOrEmpty(cryptedString)) return "";
+            string decryptedString = Encoding.UTF8.GetString(Convert.FromBase64String(cryptedString));
+            return decryptedString.Substring(0, decryptedString.Length - SecurityKey.Length);
+        }
+    }
+}
